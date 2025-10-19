953b36ae195c1a919e754eaa55100694ae024417
zsheng <zsheng.dev@gmail.com>
2025-10-19 13:32:39 +0800

    Remove unnecessary console.log statements

--- worker/bot.ts
+++ worker/bot.ts
@@ -2,9 +2,7 @@ import { Hono } from "hono";
 import { Bot, Context, webhookCallback } from "grammy";
 
 export const botApp = new Hono<{ Bindings: Env }>().post("/", async (c) => {
-  console.log("received a request");
   const bot = new Bot(c.env.BOT_TOKEN, { botInfo: JSON.parse(c.env.BOT_INFO) });
-  console.log("bot created", c.env.BOT_INFO, c.env.BOT_TOKEN);
 
   bot.command("start", async (ctx: Context) => {
     await ctx.reply("Hello, world!");
