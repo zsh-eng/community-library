60fe6045b0f98e6cef0eebe8ee78d0ff569c4b48
zsheng <zsheng.dev@gmail.com>
2025-10-25 01:46:26 +0800

    Update database migration
    Nuke the current one (temporary data anyway) and push the real one.

--- drizzle/0000_aspiring_mysterio.sql
+++ drizzle/0000_aspiring_mysterio.sql
@@ -1,14 +1,16 @@
 CREATE TABLE `book_copies` (
 	`qr_code_id` text PRIMARY KEY NOT NULL,
 	`book_id` integer NOT NULL,
+	`location_id` integer NOT NULL,
 	`copy_number` integer NOT NULL,
 	`status` text DEFAULT 'available',
-	FOREIGN KEY (`book_id`) REFERENCES `books`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`book_id`) REFERENCES `books`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`location_id`) REFERENCES `locations`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `books` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`isbn` text,
+	`isbn` text NOT NULL,
 	`title` text NOT NULL,
 	`description` text NOT NULL,
 	`author` text NOT NULL,
@@ -29,4 +31,15 @@ CREATE TABLE `loans` (
 	FOREIGN KEY (`qr_code_id`) REFERENCES `book_copies`(`qr_code_id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-CREATE INDEX `idx_active_loans` ON `loans` (`qr_code_id`,`returned_at`);
+CREATE INDEX `idx_active_loans` ON `loans` (`qr_code_id`,`returned_at`);--> statement-breakpoint
+CREATE UNIQUE INDEX `idx_unique_active_loan` ON `loans` (`qr_code_id`,`returned_at`) WHERE "loans"."returned_at" is null;--> statement-breakpoint
+CREATE TABLE `locations` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL
+);
+
+INSERT INTO `locations` (`id`, `name`) VALUES (1, 'Saga') ON CONFLICT(`id`) DO NOTHING;
+--> statement-breakpoint
+INSERT INTO `locations` (`id`, `name`) VALUES (2, 'Elm') ON CONFLICT(`id`) DO NOTHING;
+--> statement-breakpoint
+INSERT INTO `locations` (`id`, `name`) VALUES (3, 'Cendana') ON CONFLICT(`id`) DO NOTHING;
