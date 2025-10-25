b08f2f53f827b8f180baac4f4cd79d3df026dd78
zsheng <zsheng.dev@gmail.com>
2025-10-26 00:03:16 +0800

    Add system theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Book from "./Book";
 import Library from "./Library";
+import { ThemeProvider } from "./components/theme-provider";
 import { DataCacheProvider } from "./contexts/DataCacheContext";
 
 function App() {
   return (
-    <DataCacheProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Library />} />
-          <Route path="/book/:id" element={<Book />} />
-        </Routes>
-      </BrowserRouter>
-    </DataCacheProvider>
+    <ThemeProvider defaultTheme="system">
+      <DataCacheProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Library />} />
+            <Route path="/book/:id" element={<Book />} />
+          </Routes>
+        </BrowserRouter>
+      </DataCacheProvider>
+    </ThemeProvider>
   );
 }
 
