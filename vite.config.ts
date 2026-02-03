ddad5c59320011eb1212f74472ef006027b745f5
zsheng <zsheng.dev@gmail.com>
2026-02-03 23:09:06 +0800

    Change to vite dotenv loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import dotenv from "dotenv";
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-dotenv.config({ path: ".env.development" });
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), cloudflare(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), cloudflare(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    allowedHosts: [process.env.DEV_HOST!],
-  },
+    server: {
+      allowedHosts: [env.DEV_HOST!],
+    },
+  };
 });
